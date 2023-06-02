24bf516da83d2945dd9039f37a6022e9b1203b12
ariunaamy <ariun.myagmar@gmail.com>
2023-06-01 21:08:41 -0400

    icons added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import studentsData from "./data/data.json"
 import Students from "./components/Students.js"
 import Cohorts from "./components/Cohorts.js"
 import { useState } from "react"
+import { FaGithub } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
+
+
 
 function App() {
   const [studentsInCohort, setStudentsInCohort] = useState(studentsData);
@@ -16,16 +20,21 @@ function App() {
  
   return (
     <div className="body">
-      <header>
+      <header className="header">
       <h1>Student Dashboard</h1>
+      <div className="icons">
+      <a  href="https://github.com/ariunaamy"><FaGithub size={32} color="white"/></a>
+      <a  href="https://www.linkedin.com/in/ariunaa-myagmar"><FaLinkedin size={32} color="white"/></a>
+      </div>
+      
       </header>
       <main>
         <Cohorts data={studentsData} onClick={handleClick} />
         <Students data={studentsData} studentsInCohort={studentsInCohort} />
       </main>
-      <footer>
-        <p>designed by <a href="https://github.com/ariunaamy">@ariunaamy</a></p>
-      </footer>
+      {/* <footer>
+        <p></p>
+      </footer> */}
     </div>
   );
 }
